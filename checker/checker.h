1778dcb962c84710d6b7013bcf068037dffdcd72
Yan-Hao Wang <bses30074@gmail.com>
2026-01-23 09:04:04 +0000

    Add document about access files

--- checker/checker.h
+++ checker/checker.h
@@ -11,42 +11,6 @@
 int casper_check_dst_ip(const int type, struct sockaddr *sa);
 int casper_check_allowed_open(struct mac_casper *subj, struct mac_casper *obj);
 
-/* DNS */
-static const char *casper_dns_allowed_files_open[] = {
-	"/etc/nsswitch.conf", "/etc/hosts", "/etc/resolv.conf", "/etc/services",
-	NULL // Sentinel
-};
-
-/* GRP */
-static const char *grp_allowed_files_open[] = {
-	"/etc/nsswitch.conf", "/etc/group", "/var/db/cache/group.cache",
-	NULL // Sentinel
-};
-
-/* NETDB */
-static const char *netdb_allowed_files_open[] = {
-	"/etc/nsswitch.conf", "/etc/protocols",
-	NULL // Sentinel
-};
-
-/* PWD */
-static const char *pwd_allowed_files_open[] = {
-	"/etc/nsswitch.conf", "/etc/pwd.db", "/etc/spwd.db",
-	NULL // Sentinel
-};
-
-/* SYSCTL */
-static const char *sysctl_allowed_files_open[] = {
-	"/etc/pwd.db",
-	NULL // Sentinel
-};
-
-/* SYSLOG */
-static const char *syslog_allowed_files_open[] = {
-	"/var/run/log", "/dev/console", "/etc/localtime", "/etc/pwd.db",
-	NULL // Sentinel
-};
-
 /* ==========================================================
  * Policy Map
  * ========================================================== */
